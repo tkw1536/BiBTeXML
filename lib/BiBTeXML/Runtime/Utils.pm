6e903a0ba357d585656f5a4710546fcf5cb30db2
Tom Wiesing <tkw01536@gmail.com>
2018-08-22 09:42:50 -0400

    Implement changeCase Utility function

--- lib/BiBTeXML/Runtime/Utils.pm
+++ lib/BiBTeXML/Runtime/Utils.pm
@@ -14,137 +14,166 @@ use base qw(Exporter);
 our @EXPORT = (
   qw( &addPeriod ),
   qw( &changeAccent &changeCase ),
+  # TODO: Implement format.name$
+  qw( &numNames ),
 );
 
-# takes a string and adds a ‘.’ to it 
+# takes a string and adds a ‘.’ to it
 # if the last non-'}' character isn’t a ‘.’, ‘?’, or ‘!’, and pushes this resulting string.
 # implements the add.period$ built-in
 sub addPeriod {
-    my ($string) = @_;
+  my ($string) = @_;
 
-    # find the last character that is not a '}'
-    my ($match) = ($string =~ m/(.)(?:\})*$/);
+  # find the last character that is not a '}'
+  my ($match) = ($string =~ m/(.)(?:\})*$/);
 
-    # and add a '.' if it's not punctiation
-    unless ($match && ($match eq '!' or $match eq '.' or $match eq '?')){
-        return $string . '.';
-    } else {
-        return $string;
-    }
+  # and add a '.' if it's not punctiation
+  unless ($match && ($match eq '!' or $match eq '.' or $match eq '?')) {
+    return $string . '.';
+  } else {
+    return $string;
+  }
 }
 
 # changes the case of $string according to spec
 # - if $spec is 't', then upper-case the first character and lower-case the rest
 # - if $spec is 'u' then upper-case everything
 # - if $spec is 'l' then lower-case everything
-# handles accents, and ignores everything inside {s. 
+# handles accents, and ignores everything inside {s.
 # implements the change.case$ built-in
 sub changeCase {
-    my ($string, $spec) = @_;
-    $spec = lc($spec);
+  my ($string, $spec) = @_;
+  $spec = lc($spec);
 
-    # split the string into characters
-    my @characters = split(//, $string);
-    my ($level, $firstpassed) = (0, 0);
-    my $result = '';
-    my $buffer = '';
+  # split the string into characters
+  my @characters = split(//, $string);
+  my ($level, $firstpassed) = (0, 0);
+  my $result = '';
+  my $buffer = '';
 
-    # Iterate over the characters
-    my ($character, $theSpec);
-    while($character = shift(@characters)) {
-        if($level eq 0){
-            if($character eq '{'){
-                # add the backslash for sure
-                $result .= $character;
-                $level++;
+  # Iterate over the characters
+  my ($character, $theSpec);
+  while ($character = shift(@characters)) {
+    if ($level eq 0) {
+      if ($character eq '{') {
+        # add the backslash for sure
+        $result .= $character;
+        $level++;
 
-                # read the next character
-                $character = shift(@characters);
-                last unless defined($character);
-                
-                # if we do not have a backslash
-                # then put the character back and continue at level 1
-                unless($character eq '\\'){
-                    unshift(@characters, $character);
-                    $firstpassed = 1;
-                    next;
-                }
-                $result .= $character;
-                
-                # buffer everything until we are at level 0 again
-                $buffer = '';
-                $theSpec = $spec;
-                $theSpec = $firstpassed ? 'l' : 'u' if $spec eq 't';
+        # read the next character
+        $character = shift(@characters);
+        last unless defined($character);
 
-                
-                # TODO: Gobble everything until we have level 0 again
-                while(1) {
-                    $character = shift(@characters);
-
-                    # if we run out, before the bracket ends
-                    # we change the partial buffer
-                    # and then finish
-                    unless(defined($character)){
-                        $result .= changeAccent($buffer, $theSpec);
-                        return $result;
-                    }
-                    
-                    # keep reading {s
-                    if($character eq '{'){
-                        $level++;
-                    } elsif($character eq '}'){
-                        $level--;
-                        last if $level eq 0;
-                    }
-                     $buffer .= $character;
-                }
-                $result .= changeAccent($buffer, $theSpec);
-                $result .= '}';
-            } else {
-                if($spec eq 'l'){
-                    $result .= lc $character;
-                } elsif($spec eq 'u'){
-                    $result .= uc $character;
-                } else {
-                    $result .= $firstpassed ? lc $character : uc $character;
-                }
-            }
-        } else {
-            # at positive levels, do not touch any characters
-            $result .= $character;
-            if($character eq '{'){
-                $level++;
-            } elsif($character eq '}'){
-                $level--;
-            }
+        # if we do not have a backslash
+        # then put the character back and continue at level 1
+        unless ($character eq '\\') {
+          unshift(@characters, $character);
+          $firstpassed = 1;
+          next;
         }
-        $firstpassed = 1;
+        $result .= $character;
+
+        # buffer everything until we are at level 0 again
+        $buffer  = '';
+        $theSpec = $spec;
+        $theSpec = $firstpassed ? 'l' : 'u' if $spec eq 't';
+
+        # TODO: Gobble everything until we have level 0 again
+        while (1) {
+          $character = shift(@characters);
+
+          # if we run out, before the bracket ends
+          # we change the partial buffer
+          # and then finish
+          unless (defined($character)) {
+            $result .= changeAccent($buffer, $theSpec);
+            return $result;
+          }
+
+          # keep reading {s
+          if ($character eq '{') {
+            $level++;
+          } elsif ($character eq '}') {
+            $level--;
+            last if $level eq 0;
+          }
+          $buffer .= $character;
+        }
+        $result .= changeAccent($buffer, $theSpec);
+        $result .= '}';
+      } else {
+        if ($spec eq 'l') {
+          $result .= lc $character;
+        } elsif ($spec eq 'u') {
+          $result .= uc $character;
+        } else {
+          $result .= $firstpassed ? lc $character : uc $character;
+        }
+      }
+
+      # at positive levels, do not touch any characters
+    } else {
+      $result .= $character;
+      $level++ if $character eq '{';
+      $level-- if $character eq '}';
     }
-    
-    return $result;
+    $firstpassed = 1;
+  }
+
+  return $result;
 }
 
 # changes an accent (in the BiBTeX sense) either to upper or lower case
 # assumes that outer '{', '\' and '}' have already been removed
 sub changeAccent {
-    my ($accent, $spec) = @_;
+  my ($accent, $spec) = @_;
 
-    # if we have a length of 2, the second character needs to be alphabetical
-    # the first character needs to be either alphabetical or one of: ', `, ^, ", ~, =, .
-    if(length($accent) eq 2){
-        return $accent unless $accent =~ /^[a-z'`\^"~=\.][a-z]/i;
-        return $spec eq 'u' ? uc $accent : lc $accent;
-    }
-    
-    # if we have a brace, leave everything before untouched
-    # and change the case of everything after
-    my ($macro, $brace, $after) = ($accent =~ /^([^{]*)(\{)(.*)$/m);
-    if($brace){
-        return $macro . $brace . ($spec eq 'u' ? uc $after : lc $after);
+  # if we have a length of 2, the second character needs to be alphabetical
+  # the first character needs to be either alphabetical or one of: ', `, ^, ", ~, =, .
+  if (length($accent) eq 2) {
+    return $accent unless $accent =~ /^[a-z'`\^"~=\.][a-z]/i;
+    return $spec eq 'u' ? uc $accent : lc $accent;
+  }
+
+  # if we have a brace, leave everything before untouched
+  # and change the case of everything after
+  my ($macro, $brace, $after) = ($accent =~ /^([^{]*)(\{)(.*)$/m);
+  if ($brace) {
+    return $macro . $brace . ($spec eq 'u' ? uc $after : lc $after);
 
     # if we did not have a macro, return it untouched
-    } else {
-        return $accent;
-    }
+  } else {
+    return $accent;
+  }
 
+}
+
+# count the number of names inside of $string
+# i.e. the number of times the word 'and' appears surrounded by spaces
+# at brace level 0
+# implements num.names$
+sub numNames {
+  my ($string) = @_;
+
+  my $level  = 0;
+  my $result = '';
+  my $character;
+
+  # take the string and remove everything that is not of brace-level 0
+  my @characters = split(//, $string);
+  while ($character = shift(@characters)) {
+    if ($level eq 0) {
+      if ($character eq '{') {
+        $level++;
+      } else {
+        $result .= $character;
+      }
+    } else {
+      $level++ if $character eq '{';
+      $level-- if $character eq '}';
+    }
+  }
+
+  # and count the number of times we have an 'and' surrounded by spaces
+  return (() = $result =~ /\sand\s/ig) + 1;
 }
--- t/42_Runtime_utils.t
+++ t/42_Runtime_utils.t
@@ -1,5 +1,5 @@
 use BiBTeXML::Common::Test;
-use Test::More tests => 4;
+use Test::More tests => 5;
 
 subtest "requirements" => sub {
   plan tests => 1;
@@ -8,58 +8,75 @@ subtest "requirements" => sub {
 };
 
 subtest "addPeriod" => sub {
-    plan tests => 5;
+  plan tests => 5;
 
-    sub isAddPeriod {
-        my ($input, $expected) = @_;
-        is(addPeriod($input), $expected, $input);
-    }
+  sub isAddPeriod {
+    my ($input, $expected) = @_;
+    is(addPeriod($input), $expected, $input);
+  }
 
-    isAddPeriod("}}", "}}.");
-    isAddPeriod("hello world!", "hello world!");
-    isAddPeriod("hello world", "hello world.");
-    isAddPeriod("hello world}", "hello world}.");
-    isAddPeriod("hello world!}", "hello world!}");
+  isAddPeriod("}}",            "}}.");
+  isAddPeriod("hello world!",  "hello world!");
+  isAddPeriod("hello world",   "hello world.");
+  isAddPeriod("hello world}",  "hello world}.");
+  isAddPeriod("hello world!}", "hello world!}");
 };
 
 subtest "changeAccent" => sub {
-    plan tests => 6;
+  plan tests => 6;
 
-    sub isChangeAccent {
-        my ($input, $format, $expected) = @_;
-        is(changeAccent($input, $format), $expected, $format . ' => ' . $input);
-    }
+  sub isChangeAccent {
+    my ($input, $format, $expected) = @_;
+    is(changeAccent($input, $format), $expected, $format . ' => ' . $input);
+  }
 
-    isChangeAccent("ab", "u", "AB");
-    isChangeAccent("'a", "u", "'A");
-    isChangeAccent("`a", "u", "`A");
-    isChangeAccent("a0", "u", "a0");
-    isChangeAccent("hello{world}", "u", "hello{WORLD}");
-    isChangeAccent("hello", "u", "hello");
+  isChangeAccent("ab",           "u", "AB");
+  isChangeAccent("'a",           "u", "'A");
+  isChangeAccent("`a",           "u", "`A");
+  isChangeAccent("a0",           "u", "a0");
+  isChangeAccent("hello{world}", "u", "hello{WORLD}");
+  isChangeAccent("hello",        "u", "hello");
 };
 
 subtest "changeCase" => sub {
-    plan tests => 9;
+  plan tests => 9;
 
-    sub isChangeCase {
-        my ($input, $format, $expected) = @_;
-        is(changeCase($input, $format), $expected, $format . ' => ' . $input);
-    }
+  sub isChangeCase {
+    my ($input, $format, $expected) = @_;
+    is(changeCase($input, $format), $expected, $format . ' => ' . $input);
+  }
 
-    # changing case of a single world
-    isChangeCase("HeLlo", "u", "HELLO");
-    isChangeCase("HeLlo", "l", "hello");
-    isChangeCase("HeLlo", "t", "Hello");
-    
-    # case of something with brackets
-    isChangeCase("HeLlo {WeIrD} world", "u", "HELLO {WeIrD} WORLD");
-    isChangeCase("HeLlo {WeIrD} world", "l", "hello {WeIrD} world");
-    isChangeCase("HeLlo {WeIrD} world", "t", "Hello {WeIrD} world");
+  # changing case of a single world
+  isChangeCase("HeLlo", "u", "HELLO");
+  isChangeCase("HeLlo", "l", "hello");
+  isChangeCase("HeLlo", "t", "Hello");
 
-    # nested brackets
-    isChangeCase("a{b{c}}d", "u", "A{b{c}}D");
+  # case of something with brackets
+  isChangeCase("HeLlo {WeIrD} world", "u", "HELLO {WeIrD} WORLD");
+  isChangeCase("HeLlo {WeIrD} world", "l", "hello {WeIrD} world");
+  isChangeCase("HeLlo {WeIrD} world", "t", "Hello {WeIrD} world");
 
-    # accents
-    isChangeCase("{\\'a} world", "u", "{\\'A} WORLD");
-    isChangeCase("{\\0a} world", "u", "{\\0a} WORLD"); #not an accent
+  # nested brackets
+  isChangeCase("a{b{c}}d", "u", "A{b{c}}D");
+
+  # accents
+  isChangeCase("{\\'a} world", "u", "{\\'A} WORLD");
+  isChangeCase("{\\0a} world", "u", "{\\0a} WORLD");    #not an accent
+};
+
+subtest "numNames" => sub {
+  plan tests => 7;
+
+  sub isNumNames {
+    my ($input, $expected) = @_;
+    is(numNames($input), $expected, $input);
+  }
+
+  isNumNames("tom and jerry",                                           2);
+  isNumNames("and jerry",                                               1);
+  isNumNames("tom { and { and } } jerry",                               1);
+  isNumNames("jerry and",                                               1);
+  isNumNames("tom cat and jerry mouse",                                 2);
+  isNumNames("tom cat and jerry mouse and nibbles",                     3);
+  isNumNames("tom cat and jerry mouse and nibbles { and } Uncle Pecos", 3);
 };
